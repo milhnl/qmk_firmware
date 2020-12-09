7f1c2250104288765bddd32bf1238525bf19e08c
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2020-12-09 17:17:51 +0100

    Use Alt as command for Linux

--- keyboards/lily58/keymaps/milhnl/keymap.c
+++ keyboards/lily58/keymaps/milhnl/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_QUOT,KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   MT(MOD_LCTL,KC_ESC),KC_A,KC_O,KC_E, KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S,MT(MOD_RCTL,KC_MINS), \
   KC_LSFT,  KC_SCLN,KC_Q,    KC_J,    KC_K,    KC_X,  KC_LBRC, KC_RBRC,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  KC_RSFT, \
-                        MO(_LOWER), MO(_ARROW), KC_LGUI, KC_SPC, KC_ENT, KC_RGUI, KC_BSPC, MO(_MOUSE) \
+                        MO(_LOWER), MO(_ARROW), KC_LALT, KC_SPC, KC_ENT, KC_RALT, KC_BSPC, MO(_MOUSE) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
